7bd689926cc61250f426dc0a01d2de836b545b70
Sanjeev Grampurohit <sanjeev15002@gmail.com>
2021-08-31 21:17:24 -0400

    Added documentation to attackers.py

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Adversarially Robust Model for Malaria Detection 
 
-This repository utilizes an ensemble of three Convolutional Neural Networks to predict whether or not a cell is malarial from histopathological images. The diversity of the individual models provides a layer of defense against adversarial attacks using the FGSM technique. The dataset used for this study is a collection of 27588 segmented cell images acquired at Chittagong Medical College Hospital, Bangladesh. Additional details about the dataset can be found [here](https://lhncbc.nlm.nih.gov/LHC-downloads/downloads.html#malaria-datasets)
+This repository utilizes an ensemble of three Convolutional Neural Networks to predict whether or not a histopathological image of a cell is malarial. The diversity of the individual models provides a layer of defense against adversarial attacks using the FGSM technique. The dataset used for this study is a collection of 27588 segmented cell images acquired at Chittagong Medical College Hospital, Bangladesh. Additional details about the dataset can be found [here](https://lhncbc.nlm.nih.gov/LHC-downloads/downloads.html#malaria-datasets)
 
--- attackers.py
+++ attackers.py
@@ -1,14 +1,29 @@
 import numpy as np
-from PIL import Image
-import torchvision.transforms as transforms
 import tensorflow as tf
 
 class FGSMAttacker():
+    '''
+        A class for implementing the Fast Gradient Sign Method attack (Goodfellow et al, 2014) on a given model 
+    '''
     def __init__(self):
         pass
 
     @tf.function
     def get_adversarial_images(self, images,labels, model, loss_function, epsilon,num_iters):
+        '''
+            Given an input image, perturb it in accordance with FGSM
+            ======
+            Args:
+            images (np.ndarray): Numpy array of shape 
+            n_images x height x width x channels
+            labels (np.ndarray): Numpy array of shape
+            n_images x 1
+            model: A function that maps an image (shape mentioned above) to a label of shape n_classes x 1
+            loss_function: A function that returns the loss given labels and predictions
+            epsilon: Magnitude of perturbation
+            num_iters: Iteratively applies the FGSM perturbation num_iters times
+        '''
+
         delta_total = np.zeros(np.shape(images))
         for i in range(num_iters):
             y_pred = model(images)
